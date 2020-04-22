8b1ef61bd59478fb5163d102110be949812804b0
naz_dou <omelko39@gmail.com>
2020-04-22 16:22:23 +0300

    Point SDK to debug branch for Channels Implement ChannelInstance class Add alot API integration Make test's

--- src/app/sdk.service.ts
+++ src/app/sdk.service.ts
@@ -3,9 +3,9 @@ import {environment} from '../environments/environment';
 import {HttpClient} from '@angular/common/http';
 
 import Channel from '@aeternity/aepp-sdk/es/channel';
-// import {Universal} from '@aeternity/aepp-sdk/es/ae/universal';
 
 import MemoryAccount from '@aeternity/aepp-sdk/es/account/memory';
+import { unpackTx } from '@aeternity/aepp-sdk/es/tx/builder';
 import * as StringUtils from '@aeternity/aepp-sdk/es/utils/string';
 
 
@@ -23,6 +23,7 @@ enum ActionTypes {
   providedIn: 'root'
 })
 export class SdkService {
+  private $channel;
   public initiatorAccount = MemoryAccount({
     keypair: {
       publicKey: 'ak_SVQ9RvinB2E8pio2kxtZqhRDwHEsmDAdQCQUhQHki5QyPxtMh',
@@ -30,36 +31,102 @@ export class SdkService {
       secretKey: '5245D200D51B048C825280578EDDA2160F48859D49DCFC3510D87CC46758C97C39E09993C3D5B1147F002925270F7E7E112425ABA0137A6E8A929846A3DFD871'
     }
   });
-  channelParams
   wsUrl: string = environment.SC_NODE_URL;
   backendServiceUrl: string = environment.BACKEND_SERVICE_URL;
 
 
   constructor(private http: HttpClient) {
   }
-  signTx(tag, tx) {
-    if (confirm('Do you want to sign this tx -> ' + tag)) { return this.initiatorAccount.signTransaction(tx); }
-    return null;
+
+  get channel() {
+    return this.$channel;
   }
 
-  async getConnectionParams(params: { address: string, port: number }): Promise<any> {
-    return this.http.get(`${this.backendServiceUrl}/connect/new?client_account=${params.address}&port=${params.port}`).toPromise();
-  }
-
-  async initChannel(params: any) {
-    const address = await this.initiatorAccount.address();
-    const channelConfig = await this.getConnectionParams({address, port: 1600});
-    this.channelParams = Object
-      .entries({ ...channelConfig.expected_initiator_configuration.basic, ...channelConfig.expected_initiator_configuration.custom })
+  async getChannelConfig(params: { address: string, port: number }): Promise<any> {
+    // Get config from backend
+    const configFromBackendService: any = await this.http
+      .get(`${this.backendServiceUrl}/connect/new?initiator_id=${params.address}&port=${params.port}`)
+      .toPromise();
+    // Prepare the config
+    return Object
+      .entries(configFromBackendService.expected_initiator_configuration)
       .reduce((acc, [key, value]) => ({ ...acc, [StringUtils.snakeToPascal(key)]: value }), { url: this.wsUrl });
-    console.log('Channel params', this.channelParams)
-    const channelInstance = await Channel({
+  }
+
+  async initChannel(params: any = {}) {
+    const address = await this.initiatorAccount.address();
+    const channelConfig = await this.getChannelConfig({ address, port: 1564 });
+
+    this.$channel = new ChannelInstance(channelConfig, this.initiatorAccount);
+    await this.channel.openChannel();
+    return this.channel;
+  }
+}
+
+export class ChannelInstance {
+  private $channel;
+  private $initiatorAccount;
+  channelParams;
+  networkId: string;
+  opened;
+
+  constructor(params, account, { networkId } = { networkId: 'ae_channel_service_test' }) {
+    this.channelParams = params;
+    this.$initiatorAccount = account;
+    this.networkId = networkId;
+  }
+
+  get channel() {
+    return this.$channel;
+  }
+
+  async signTx(tag, tx) {
+    console.log('Channel signing -----------');
+    console.log('Signing using networkId -> ' + this.networkId);
+    console.log('Channel sign tag -> ' + tag);
+    console.log('Channel sign transaction: ', unpackTx(tx));
+    console.log('---------------------------');
+    return this.$initiatorAccount.signTransaction(tx, { networkId: this.networkId });
+  }
+
+  async openChannel() {
+    this.$channel = await Channel({
       ...this.channelParams,
-      // ...params,
-      sign: this.signTx
+      sign: this.signTx.bind(this),
+      // debug: true
     });
-    await this.waitForChannelStart(channelInstance);
-    return channelInstance;
+    // Register round handler
+    // Update round in local storage for each change
+    this.$channel.on('stateChanged', async (newState) => {
+      localStorage.setItem('fsmId', this.channel.fsmId());
+      localStorage.setItem('state', JSON.stringify({ stateTx: newState, offChainState: await this.channel.state() }));
+      localStorage.setItem('round', this.channel.round());
+    });
+    this.$channel.on('statusChanged', (status) => {
+      localStorage.setItem('status', this.channel.status());
+      if (status === 'open') {
+        localStorage.setItem('channel', JSON.stringify({
+          params: this.channelParams,
+          id: this.channel.id()
+        }));
+      }
+    });
+  }
+
+  onOpened(callback) {
+    if (!this.channel) {
+      throw new Error('Channel create process is npt started. Please run `openChannel()`');
+    }
+    if (this.opened) {
+      throw new Error('Channel already opened');
+    }
+    // Awaiting of channel open
+    return this.$channel.on('statusChanged', (status) => {
+      if (status === 'open') {
+        this.opened = true;
+        callback();
+      }
+    });
   }
 
   /**
@@ -76,19 +143,31 @@ export class SdkService {
    *   - "statusChanged"
    *
    */
-  onAction(channel, action: ActionTypes, callback: (msg: any) => void) {
-    return channel.on(action, callback);
+  onAction(action: ActionTypes, callback: (msg: any) => void) {
+    if (!this.channel) {
+      throw new Error('Channel not opened');
+    }
+    return this.channel.on(action, callback);
   }
 
-  async waitForChannelStart(channel) {
-    return new Promise(resolve =>
-      this.onAction(channel, ActionTypes.statusChanged, (status) => {
-        if (status === 'open') {
-          resolve();
-        }
-      })
-    );
+  async deposit(amount: number | string) {
+      return await this.channel.deposit(amount, tx => this.signTx('deposit_tx', tx));
   }
 
+  async transfer(from: string, to: string, amount: number | string) {
+    return await this.channel.update(from, to, amount, tx => this.signTx('deposit_tx', tx));
+  }
 
+  async withdrawal(amount: number | string) {
+    return await this.channel.withdrawal(amount, tx => this.signTx('withdrawal_tx', tx));
+  }
+
+  disconnect(amount: number | string) {
+    this.channel.disconnect();
+  }
+
+  async closeChannel() {
+    this.channel.disconnect();
+    return this.channel.shutdown(tx => this.signTx('shutdown_tx', tx));
+  }
 }
--- src/app/transactionlist/transactionlist.component.ts
+++ src/app/transactionlist/transactionlist.component.ts
@@ -4,7 +4,7 @@ import { SdkService } from '../sdk.service'
 @Component({
   selector: 'app-transactionlist',
   templateUrl: './transactionlist.component.html',
-  styleUrls: ['../../../ArrowNavigationStyles/css/demo.css','../../../ArrowNavigationStyles/css/component.css','../../../ArrowNavigationStyles/css/normalize.css']
+  styleUrls: ['../../../ArrowNavigationStyles/css/demo.css', '../../../ArrowNavigationStyles/css/component.css', '../../../ArrowNavigationStyles/css/normalize.css']
 })
 export class TransactionlistComponent implements OnInit {
 
@@ -17,15 +17,17 @@ export class TransactionlistComponent implements OnInit {
   }
 
   initChannelAndMakeTransfer() {
-    this.sdkService.initChannel({}).then(channel => {
-      // here channel already initialized
-      // Make transfer
-      channel.update(
-        this.sdkService.initiatorAccount.address(),
-        this.sdkService.channelParams.responderId,
-        10000,
-        this.sdkService.signTx
-      ).then(res => { debugger }).error(error => { debugger })
+    this.sdkService.initChannel().then(async (channel) => {
+      channel.onOpened(async () => {
+        // here channel already opened
+        // When channel is created backend service deploy the contract
+        // this timeout will wait until this happens to avoid conflicts
+        setTimeout(async () => {
+          const balanceBefore = await channel.channel.balances([channel.channelParams.initiatorId, channel.channelParams.responderId]);
+          const transfer = await channel.transfer(channel.channelParams.initiatorId, channel.channelParams.responderId, 100000);
+          const balanceAfter = await channel.channel.balances([channel.channelParams.initiatorId, channel.channelParams.responderId]);
+        }, 15000);
+      });
     }).catch(e => { debugger });
   }
 
