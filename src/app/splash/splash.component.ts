76371bf9c47dc106ed5d332c2354d6de2ab61af3
naz_dou <omelko39@gmail.com>
2020-05-11 16:59:48 +0300

    Small refactor of SDK service and game component

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,6 +1,16 @@
 import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
 import {SdkService} from '../sdk.service';
 
+const randomString = (len: number, charSet?: string) => {
+  charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return (new Array(len))
+    .reduce(
+      (acc, _, i) => {
+        const randomPoz = Math.floor(Math.random() * charSet.length);
+        return acc.concat(charSet.substring(randomPoz, randomPoz + 1));
+      }, '');
+}
+
 enum State {
   initial = 'initial',
   channelCreated = 'channelCreated',
@@ -34,68 +44,6 @@ export class SplashComponent implements OnInit {
 
   constructor(private sdkService: SdkService, private changeDetectorRef: ChangeDetectorRef) {
     this.state = State.initial;
-  }
-
-  updateState(newState: State): void {
-    this.state = newState;
-    this.changeDetectorRef.detectChanges();
-  }
-
-  async closeGame() {
-    const shutdown = await this.sdkService.channel.closeChannel();
-    console.log('--------------- Channel shutdown complete ---------------', shutdown);
-    this.updateState(State.closed);
-  }
-
-  openChannel() {
-    this.initChannelAndWaitForContract();
-    this.updateState(State.initial);
-  }
-
-  startRound() {
-    this.salt = null;
-    this.guess = null;
-    this.updateState(State.hashInserted);
-  }
-
-  async updateBalance() {
-    this.balance = await this.sdkService.channel.getBalance();
-  }
-
-  goToLobby() {
-    this.updateState(State.lobby);
-  }
-
-  randomString(len: number, charSet?: string) {
-    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let randomString = '';
-    for (let i = 0; i < len; i++) {
-      const randomPoz = Math.floor(Math.random() * charSet.length);
-      randomString += charSet.substring(randomPoz, randomPoz + 1);
-    }
-    return randomString;
-  }
-
-  async playRound(guess: string) {
-    try {
-      this.guess = guess;
-      this.salt = this.randomString(25);
-      const hash = await this.sdkService.channel.contractDryRun('compute_hash', this.contractAddress, [`"${this.salt}"`, `"${guess}"`]);
-      console.log(this.stake)
-      // tslint:disable-next-line:max-line-length
-      const providedHashResult = await this.sdkService.channel.contractCall('provide_hash', this.contractAddress, [`${hash}`], { amount: this.stake });
-      const casinoPickResult = await this.sdkService.channel.awaitContractCall('casino_pick');
-      const revealRes = await this.sdkService.channel.contractCall('reveal', this.contractAddress,  [`"${this.salt}"`, `"${guess}"`]);
-      await this.updateBalance();
-      casinoPickResult.decoded.arguments[0].value !== this.guess ? this.updateState(State.won) : this.updateState(State.lost);
-    } catch (err) {
-      console.log(err);
-      this.updateState(State.error);
-    }
-  }
-
-  async setGuess(guess: string) {
-    this.playRound(guess);
   }
 
   initChannelAndWaitForContract() {
@@ -146,5 +94,65 @@ export class SplashComponent implements OnInit {
 
   ngOnInit() {
     this.initChannelAndWaitForContract();
+  }
+
+  updateState(newState: State): void {
+    this.state = newState;
+    this.changeDetectorRef.detectChanges();
+  }
+
+  openChannel() {
+    this.initChannelAndWaitForContract();
+    this.updateState(State.initial);
+  }
+
+  startRound() {
+    this.salt = null;
+    this.guess = null;
+    this.updateState(State.hashInserted);
+  }
+
+  goToLobby() {
+    this.updateState(State.lobby);
+  }
+
+  async playRound(guess: string) {
+    let step = 0;
+    try {
+      this.guess = guess;
+      this.salt = randomString(25);
+      const hash = await this.sdkService.channel.contractDryRun('compute_hash', this.contractAddress, [`"${this.salt}"`, `"${guess}"`]);
+      // tslint:disable-next-line:max-line-length
+      const providedHashResult = await this.sdkService.channel.contractCall('provide_hash', this.contractAddress, [`${hash}`], { amount: this.stake });
+      step += 1;
+      const casinoPickResult = await this.sdkService.channel.awaitContractCall('casino_pick');
+      step += 1;
+      const revealRes = await this.sdkService.channel.contractCall('reveal', this.contractAddress,  [`"${this.salt}"`, `"${guess}"`]);
+      step += 1;
+      await this.updateBalance();
+      casinoPickResult.decoded.arguments[0].value !== this.guess ? this.updateState(State.won) : this.updateState(State.lost);
+    } catch (err) {
+      switch (step) {
+        // Provide hash
+        case 1:
+        // Casino pick
+        case 2:
+        // Reveal
+        case 3:
+        default:
+          console.log(err);
+          this.updateState(State.error);
+      }
+    }
+  }
+
+  async closeGame() {
+    const shutdown = await this.sdkService.channel.closeChannel();
+    console.log('--------------- Channel shutdown complete ---------------', shutdown);
+    this.updateState(State.closed);
+  }
+
+  async updateBalance() {
+    this.balance = await this.sdkService.channel.getBalance();
   }
 }
